416a51e6d7984c40cd0391bd46ef9adb7ffc19ab
Volodymyr Franchuk <franchuk.vldr@gmail.com>
2023-02-09 02:57:59 +0200

    Added horizontal-parallax

---
+++ horizontal-parallax/js/app.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+	// direction: 'vertical',
+	speed: 5000,
+	spaceBetween: 18,
+	parallax: true,
+	freeMode: true,
+	mousewheel: {
+		enabled: true,
+		sensitivity: 5
+	},
+});
